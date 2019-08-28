fb0ba04d152078c9b27c9ae65a9affcbb0fc0133
Conan Adams <Conanbbc1999@gmail.com>
2019-08-28 12:17:27 +0300

    first commit

---
+++ checkthegest.py
@@ -0,0 +1,52 @@
+##this program checks whethe__init__r a guest can enter a party iff he/she satifies the following condidtons
+##a. guest must be Victoria  studentff ['[  1#b. be in the age range 18-25
+##c. must come with opposite sex (hot partner at that)
+
+print('''
+        ===========================================================
+            WELCOME PARTY FREAK! READY TO GET YOUR GROOVE ON?
+        ===========================================================
+        ''')
+print('\nBut before you enter, we have to make sure you satify all our conditions')
+
+
+def victoriaStudent():
+    yourSchool = input('\nEnter the name of the school you attend: ')
+    yourSchool = yourSchool.lower()
+    return 'Victoria' in yourSchool
+
+
+def ageRange():
+    age = int(input('\nEnter your age: '))
+    return age in range(19, 25)
+
+
+def withSomeone():
+    withDate = input('\nAre you with a date?(Yes/No)')
+    withDate = withDate.lower()
+    if withDate == 'yes':
+        dateGender = input('Is your date of opposite gender?(Yes/No)')
+        dateGender = dateGender.lower()
+        if dateGender == 'yes':
+            dateRate = int(input('On a scale of 1-10 how would you rate your date?'))
+            return dateRate > 7
+
+
+def guestList():
+    yourName = input('\nEnter your name: ')
+    schResponse = victoriaStudent()
+    ageResponse = ageRange()
+    dateResponse = withSomeone()
+
+    if schResponse and ageResponse and dateResponse:
+        print('We have been expecting you, {}'.format(yourName))
+    else:
+        print('Sorry, you are not allowed into this party')
+
+
+if __name__ == '__main__':
+    guestList()
+
+
+
+
